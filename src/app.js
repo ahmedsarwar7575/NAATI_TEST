0199959e7af3efe757f644789b0958c28d587371
ahmedsarwar7575 <ahmedsarwar7575@gmail.com>
2026-01-16 16:15:21 +0500

    Cors

--- src/app.js
+++ src/app.js
@@ -3,15 +3,20 @@ import cors from "cors";
 import { apiRouter } from "./routes/index.js";
 import { notFound, errorHandler } from "./middleware/error.js";
 import { stripeWebhook } from "./controllers/stripe.controller.js";
+
 export const app = express();
 
 app.use(cors());
 app.options("*", cors());
+
+app.options("/api/v1/stripe/webhook", cors());
+
 app.post(
   "/api/v1/stripe/webhook",
   express.raw({ type: "application/json" }),
   stripeWebhook
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/v1", apiRouter);
