c737906ac4e65b6f355c0ac5c72619f7ac1d37af
ahmedsarwar7575 <ahmedsarwar7575@gmail.com>
2026-01-21 01:55:28 +0500

    DATA MIGRATION

--- src/controllers/dialogues.controller.js
+++ src/controllers/dialogues.controller.js
@@ -34,7 +34,6 @@ export async function listDialogues(req, res, next) {
 
     const dialogues = await models.Dialogue.findAll({
       where,
-      order: [["id", "DESC"]],
       include: [{ model: models.Domain }, { model: models.Language }]
     });
 
